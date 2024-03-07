3e7109eab33fde40b5407c39d18bdcccbb430a5d
Shraddhanage8980 <132291066+Shraddhanage8980@users.noreply.github.com>
2024-03-07 22:37:24 +0530

    task-02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  // standalone: true,
+  // imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Task-2';
+
+  showPhoneNUmber = false;
+  showEmailId = false;
+  name : any = ["Phone","Email"]
+  @ViewChild('inputEle') inputEle : ElementRef | undefined;
+  Onclick(){
+    let selectValue = this.inputEle?.nativeElement.value;
+    this.showPhoneNUmber = selectValue === "Phone";
+    this.showEmailId = selectValue === "Email";
+  }
 }
